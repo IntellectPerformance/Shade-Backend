979032fb6b7ecbe8e311f9d4724b345c724c2086
Milan <41877233+IntellectPerformance@users.noreply.github.com>
2022-05-29 20:00:48 +0200

    Code Polishment in Registration

--- app/Controllers/User/Auth/Registration.php
+++ app/Controllers/User/Auth/Registration.php
@@ -13,31 +13,33 @@ class Registration extends BaseController
     public function create()
     {
         $rules = [
-            'username'  => 'required|min_length[4]|max_length[20]|pattern[[a-zA-Z0-9-=?!@:.]+]|is_unique[users.username]',
-            'email'     => 'required|valid_email|is_unique[users.mail]',
-            'password'  => 'required|min_length[6]',
-            'password_confirmation'=> 'required|matches[password]'
+            'username'          => 'required|min_length[4]|max_length[20]|pattern[[a-zA-Z0-9-=?!@:.]+]|is_unique[users.username]',
+            'email'             => 'required|valid_email|is_unique[users.mail]',
+            'password'          => 'required|min_length[6]',
+            'password_confirmation' => 'required|matches[password]'
         ];
 
         $data = [
-            'username'  => $this->request->getVar('username'),
-            'mail'      => $this->request->getVar('email'),
-            'password'  => $this->request->getVar('password'),
-            'motto'     => $this->request->getVar('motto'),
-            'account_created' => time(),
+            'username'          => $this->request->getVar('username'),
+            'mail'              => $this->request->getVar('email'),
+            'password'          => $this->request->getVar('password'),
+            'motto'             => $this->request->getVar('motto'),
+            'account_created'   => time(),
 			
-			// User Registration Reward 
-			'credits' => 50000, 
-			'pixels' => 500,
-			'points' => 15,
+	    // User Registration Reward 
+	    'credits'           => 50000, 
+	    'pixels'            => 500,
+	    'points'            => 15,
 			
-            'ip_register'     => $_SERVER['REMOTE_ADDR'],
-            'ip_current'      => $_SERVER['REMOTE_ADDR']
+            'ip_register'       => $_SERVER['REMOTE_ADDR'],
+            'ip_current'        => $_SERVER['REMOTE_ADDR']
         ];
 
         $errors = service('validate')->run($this->validate($rules));
         if($errors) {
-            return redirect()->back()->with('errors', $errors);
+            return redirect()->back()->with(
+		    'errors', 
+		    $errors);
         }
 
         $user = $this->userModel->insert($data);
@@ -46,14 +48,14 @@ class Registration extends BaseController
             $this->userModel->find($user)
         );
 
-        return redirect()->to('me')->with('success', lang('Your Account Is Created'));
+        return redirect()->to('me')->with(
+		'success', 
+		lang('Your Account Is Created'));
     }
 
     public function index()
     {
 
-        echo view('user/auth/register');
-
-
+       return view('user/auth/register');
     }
 }
