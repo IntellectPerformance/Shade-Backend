fabcc830ce183d796043cdce9d16559270ff3b0e
Milan <41877233+IntellectPerformance@users.noreply.github.com>
2022-05-29 20:04:26 +0200

    Admin Filter Code Polishment

--- app/Filters/AdminFilter.php
+++ app/Filters/AdminFilter.php
@@ -17,15 +17,22 @@ class AdminFilter implements FilterInterface
 
       if(session()->has('user'))
         {
-          $user = $this->userModel->where('username', $this->session->get('user')->username)->first();
+          $user = $this->userModel->where(
+              'username', 
+              $this->session->get('user')->username)->first();
+          
           if ($user->rank < getenv('min_hk_rank'))
             {
-              return redirect()->to('me')->with('errors', 'Not enough permissions');
+              return redirect()->to('me')->with(
+                  'errors', 
+                  'Not enough permissions');
             }
         }
-        // For now I'll do this, in the next ShadeCMS update I will improve the code structure.
+        
         if(!session()->has('user')) {
-          return redirect()->to('/')->with('errors', 'No logged in user found');
+          return redirect()->to('/')->with(
+              'errors', 
+              'No logged in user found');
         }
     }
 
